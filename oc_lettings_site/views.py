c82d140ad5245c15f68e655880a2c36cce149aaf
Xavier Lonjon <xavierlonjon@yahoo.fr>
2024-01-18 15:40:25 -0600

    adding docstring

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 
 
-# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros,
-# vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum, eget consequat ipsum lobortis quis. Phasellus eleifend ex auctor venenatis tempus.
-# Aliquam vitae erat ac orci placerat luctus. Nullam elementum urna nisi, pellentesque iaculis enim cursus in. Praesent volutpat porttitor magna, non finibus neque cursus id.
 def index(request):
+    """
+    This function returns the index.html template when a GET request is made to the root URL.
+    """
     return render(request, 'index.html')
 
 
 def handler500(request):
+    """
+    This function returns the 500.html template when a GET request is made to a URL that does not exist.
+    """
     return render(request, '500.html', status=500)
 
 def handler404(request, exception):
+    """
+    This function returns the 404.html template when a GET request is made to a URL that does not exist.
+    """
     return render(request, '404.html', status=404)
