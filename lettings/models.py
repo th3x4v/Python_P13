9d21b2bd1b99e37d0595958e23fb07ef3d83a1b1
Xavier Lonjon <xavierlonjon@yahoo.fr>
2024-01-18 12:37:40 -0600

    correction of address model display

--- lettings/models.py
+++ lettings/models.py
@@ -4,6 +4,9 @@ from django.core.validators import MaxValueValidator, MinLengthValidator
 
 
 class Address(models.Model):
+    class Meta:
+        verbose_name = "Address"
+        
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
