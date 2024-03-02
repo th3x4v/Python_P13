3e75fe201d09dad9babdc31723802a11580509e9
Xavier Lonjon <xavierlonjon@yahoo.fr>
2024-03-02 23:33:43 +0100

    update models documentation

--- lettings/models.py
+++ lettings/models.py
@@ -3,14 +3,16 @@ from django.core.validators import MaxValueValidator, MinLengthValidator
 
 
 class Address(models.Model):
+    """
+    This model defines the Address class, which represents a physical mailing address.
+    """
+
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
     state = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
     zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
-    country_iso_code = models.CharField(
-        max_length=3, validators=[MinLengthValidator(3)]
-    )
+    country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
 
     class Meta:
         verbose_name = "Address"
@@ -21,6 +23,12 @@ class Address(models.Model):
 
 
 class Letting(models.Model):
+    """
+    This model defines the Letting class, which represents a rental property
+    listing.
+    It has a title and a foreign key reference to an Address object.
+    """
+
     title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
 
--- profiles/models.py
+++ profiles/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    """
+    This model contains user profile information associated with a User account.
+    """
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     favorite_city = models.CharField(max_length=64, blank=True)
 
