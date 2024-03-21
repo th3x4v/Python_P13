37300bf8c4b45ce5114addf1150869d71d0ff506
Xavier Lonjon <xavierlonjon@yahoo.fr>
2024-03-21 14:39:49 +0100

    bug env var DEBUG

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG_ENV", True)
+DEBUG = os.getenv("DEBUG", "False") == "True"
+# DEBUG = os.getenv("DEBUG_ENV", True)
 
 print(DEBUG)
 print("DEBUG")
